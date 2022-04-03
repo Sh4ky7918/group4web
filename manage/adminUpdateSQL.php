7f9b09b76bc85e980c707bc37a32799ecfe09b86
Sh4ky7918 <98089829+Sh4ky7918@users.noreply.github.com>
2022-04-04 00:42:42 +0100

    Update adminUpdateSQL.php

--- manage/adminUpdateSQL.php
+++ manage/adminUpdateSQL.php
@@ -7,10 +7,18 @@ You need $_POST['firstName'], $_POST['lastName'] and $_POST['Role'] for the deta
 Look at all the other php files with SQL on them if you need a reference to what it should look like and where you should put the variables.
 
 -->
-<?php
-function updateUserFunc(){
-
+function updateUser()
+{
+<!--
+//q = Query
+//cq Compound Query
+-->
+	$q = "UPDATE `User` SET `firstName`=:fName, `lastName`=:lName, `Role`=:role where `ID` = :ID";
+	$cq = REPLACEMEWITHYOURDBCONNECTION->prepare($q);
+	$cq->bindValue(':fName',$_SESSION['firstName']);
+	$cq->bindValue(':lName',$_SESSION['lastName']);
+	$cq->bindValue(':role',$_SESSION['Role']);
+	$cq->bindValue(':ID',$_SESSION['userID']);
+	$res = $cq->execute();
 }
-
-
 ?>
